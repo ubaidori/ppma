fe88a725440e6c086d55b2e072b17c903db07caf
ubaidori <imubaidori@gmail.com>
2025-04-30 08:07:54 +0700

    h2

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\News;
 use Illuminate\Contracts\View\View;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 
 class NewsController extends Controller
@@ -13,5 +15,31 @@ class NewsController extends Controller
         $news = News::latest()->paginate(5);
 
         return view('news.index', compact('news'));
+    }
+
+    public function create(): View
+    {
+        return view('news.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = Validator::make($request->all(), [
+            'image'     => 'required|image|mimes:jpeg,jpg,png|max:2048',
+            'title'     => 'required|min:5',
+            'content'   => 'required|min:10'
+        ])->validate();
+
+        $image = $request->file('image');
+        // dd($image);
+        $image->storeAs('news', $image->hashName());
+
+        News::create([
+            'image'     => $image->hashName(),
+            'title'     => $request->title,
+            'content'   => $request->content
+        ]);
+
+        return redirect()->route('news.index')->with(['success' => 'Data Berhasil Disimpan']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\NewsController;
 use App\Http\Controllers\StructureController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,4 @@ Route::get('/', function () {
 });
 
 Route::resource('/structures', StructureController::class);
+Route::resource('/news', NewsController::class);
